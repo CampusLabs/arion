e4cb091b94cef7effb9a925e1d04bb8406c5f417
Joshua Griffith <joshua@orgsync.com>
2016-02-16 19:17:46 -0600

    Release under Apache 2.0 license

--- README.md
+++ README.md
@@ -195,7 +195,19 @@ specified in the `STATSD_HOST` and `STATSD_PORT` environment variables. The
 
 ## License
 
-Copyright © 2016 OrgSync.
+Copyright 2016 OrgSync.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 [Arion]: https://en.wikipedia.org/wiki/Arion_(mythology)
 [DGRBM]: http://www.theoi.com/Ther/HipposAreion.html
--- project.clj
+++ project.clj
@@ -1,7 +1,22 @@
-(defproject arion "0.1.0-SNAPSHOT"
+;Copyright 2016 OrgSync
+;
+;Licensed under the Apache License, Version 2.0 (the "License");
+;you may not use this file except in compliance with the License.
+;You may obtain a copy of the License at
+;
+;http://www.apache.org/licenses/LICENSE-2.0
+;
+;Unless required by applicable law or agreed to in writing, software
+;distributed under the License is distributed on an "AS IS" BASIS,
+;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;See the License for the specific language governing permissions and
+;limitations under the License.
+
+(defproject arion "1.0.7"
   :description "Talks to Kafka so you don't have to"
   :url "https://github.com/orgsync/arion"
-  :license {:name "Copyright 2016 OrgSync."}
+  :license {:name "Apache 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[aleph "0.4.1-beta3"]
                  [bidi "1.25.1"]
                  [byte-streams "0.2.1-alpha1"]
