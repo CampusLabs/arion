578b2d5168ff95c5b2dc65e2f488304bbb481850
Joshua Griffith <joshua@orgsync.com>
2016-02-21 21:36:55 -0600

    Bump version

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
 ;See the License for the specific language governing permissions and
 ;limitations under the License.
 
-(defproject arion "1.1.0"
+(defproject arion "1.1.3-SNAPSHOT"
   :description "Talks to Kafka so you don't have to"
   :url "https://github.com/orgsync/arion"
   :license {:name "Apache 2.0"
