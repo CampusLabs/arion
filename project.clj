6438eba0ffc8f0bcc02b94afe08bb871612ca33b
Joshua Griffith <joshua@orgsync.com>
2016-03-19 19:42:37 -0500

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -12,18 +12,18 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject arion "1.1.3-SNAPSHOT"
+(defproject arion "1.1.8"
   :description "Talks to Kafka so you don't have to"
   :url "https://github.com/orgsync/arion"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[aleph "0.4.1-beta3"]
-                 [bidi "1.25.1"]
-                 [byte-streams "0.2.1-alpha1"]
+  :dependencies [[aleph "0.4.1-beta5"]
+                 [bidi "2.0.4"]
+                 [byte-streams "0.2.1"]
                  [camel-snake-kebab "0.3.2"]
                  [com.basistech/metrics-statsd "3.0.0"]
                  [com.stuartsierra/component "0.3.1"]
-                 [com.taoensso/timbre "4.2.1"]
+                 [com.taoensso/timbre "4.3.1"]
                  [danlentz/clj-uuid "0.1.6"]
                  [environ "1.0.2"]
                  [factual/durable-queue "0.1.5"]
@@ -32,7 +32,7 @@
                  [metrics-clojure-jvm "2.6.1"]
                  [org.apache.kafka/kafka-clients "0.9.0.1"]
                  [org.clojure/clojure "1.8.0"]
-                 [pjson "0.3.1"]]
+                 [pjson "0.3.2"]]
   :main arion.core
   :uberjar-name "arion.jar"
   :profiles {:dev     {:dependencies [[criterium "0.4.4"]]}
